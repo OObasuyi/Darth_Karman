43922cc4520a3a7d6cb12bacfbaf07e7f6d34070
Osamuede Obasuyi <80005871+collardGreensQ@users.noreply.github.com>
2021-06-08 10:48:02 -0400

    Update login_network_devices.py

--- login_network_devices.py
+++ login_network_devices.py
@@ -1,22 +1,24 @@
 from netmiko import ConnectHandler
+from ftd_api.ftd_client import FTDClient
 import file_control as fc
 import json
 
 class ConnHandler:
-    def __init__(self):
-        self.cred_dict = self.pull_creds()
+    def __init__(self,type_:str):
+        self.cred_dict = self.pull_creds(type_)
 
-    def pull_creds(self):
-        creds = fc.get_file('cHolder.json', 'json')
-        if creds is None:
+    def pull_creds(self,type_):
+        try:
+            creds = fc.get_file('cHolder.json', 'json')
+            cdict = json.load(creds)
+            typedata = cdict[type_]
+            return typedata
+        except:
             username_input = input("USERNAME:")
             userpasswd_input = input("PASSWORD:")
-            cdict = {'username': username_input, 'password': userpasswd_input}
-            with open('cHolder.json', 'w', encoding='utf-8') as fj:
+            cdict = {type_: {'username': username_input, 'password': userpasswd_input}}
+            with open('cHolder.json', 'a+', encoding='utf-8') as fj:
                 json.dump(cdict, fj, ensure_ascii=False, indent=4)
-            return cdict
-        else:
-            cdict = json.load(creds)
             return cdict
 
     def login_ios_def(self,ip_addess,uname,passwd):
@@ -29,5 +31,15 @@ class ConnHandler:
         ios_device = ConnectHandler(**ios_device_details)
         return ios_device
 
+    def login_fpr_def(self,ip_address,uname,passwd,port:int = 443):
+        try:
+            client = FTDClient(address=ip_address,port=port,username=uname,password=passwd)
+            client.login()
+            return client
+        except Exception as err:
+            print(f'ERROR: {err}')
+            raise
+
+
 if __name__ == "__main__":
-    ch = ConnHandler()
+    ch = ConnHandler('test9')
